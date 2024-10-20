0c0444fe3eb479be5c763c7016a87fe58a812c1f
AggelosTh <aggtheo@hotmail.com>
2024-10-20 21:04:59 +0300

    Create player and move it in the map

---
+++ entities.py
@@ -0,0 +1,74 @@
+import pygame
+import pytmx
+from pygame.locals import *
+
+width = 40
+height = 30
+tilewidth = 32
+tileheight = 16
+
+GREEN = (0, 255, 0)
+
+screen_width = width * tilewidth
+screen_height = height * tileheight
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+class TiledMap:
+    def __init__(self, filename: str):
+        tm = pytmx.load_pygame(filename, pixelalpha=True)
+        self.width = tm.width * tm.tilewidth
+        self.height = tm.height * tm.tileheight
+        self.tmxdata = tm
+
+    def render(self, surface: pygame.Surface):
+        for layer in self.tmxdata.visible_layers:
+            if isinstance(layer, pytmx.TiledTileLayer):
+                for x, y, gid, in layer:
+                    tile = self.tmxdata.get_tile_image_by_gid(gid)
+                    if tile:
+                        # This block converts the map to ismetric. If we want to disable it
+                        # we need to comment the following code and uncomment the next one.
+                        tile = tile.convert_alpha()
+                        screen_x = (x - y) * (tilewidth // 2) + (screen_width // 2)
+                        screen_y = (x + y) * (tileheight // 2)
+                        surface.blit(tile, (screen_x, screen_y + 20))
+                        # surface.blit(tile, (x * self.tmxdata.tilewidth,
+                        #                     y * self.tmxdata.tileheight))
+
+    def make_map(self, screen):
+        # temp_surface = pygame.Surface((self.width, self.height))
+        self.render(screen)
+        # return temp_surface
+
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super().__init__()
+        self.image = pygame.image.load('geralt.bmp').convert_alpha()  # Load player sprite
+        self.rect = self.image.get_rect()
+        self.map_x = x
+        self.map_y = y
+        self.speed = 1
+
+    def move(self, dx, dy):
+        self.map_x += dx
+        self.map_y += dy
+
+    def update(self, keys):
+        dx = dy = 0
+        if keys[K_LEFT]:
+            dx = -self.speed
+        if keys[K_RIGHT]:
+            dx = self.speed
+        if keys[K_UP]:
+            dy = -self.speed
+        if keys[K_DOWN]:
+            dy = self.speed
+        
+        # Adjust movement for isometric map
+        self.move(dx, dy)
+
+        self.rect.x = (self.map_x - self.map_y) * (tilewidth // 2) + (tilewidth // 2) - self.image.get_width() // 2
+        self.rect.y = (self.map_x + self.map_y) * (tileheight // 2) - self.image.get_height()
--- map.py
+++ map.py
@@ -1,48 +1,16 @@
 import pygame
-import pytmx
+from entities import TiledMap, Player, screen
 
 # Initialize Pygame
 pygame.init()
-
-width = 40
-height = 30
-tilewidth = 32
-tileheight = 16
-
-screen_width = width * tilewidth
-screen_height = height * tileheight
-screen = pygame.display.set_mode((screen_width, screen_height))
-
-
-class TiledMap:
-    def __init__(self, filename: str):
-        tm = pytmx.load_pygame(filename, pixelalpha=True)
-        self.width = tm.width * tm.tilewidth
-        self.height = tm.height * tm.tileheight
-        self.tmxdata = tm
-
-    def render(self, surface: pygame.Surface):
-        for layer in self.tmxdata.visible_layers:
-            if isinstance(layer, pytmx.TiledTileLayer):
-                for x, y, gid, in layer:
-                    tile = self.tmxdata.get_tile_image_by_gid(gid)
-                    if tile:
-                        # This block converts the map to ismetric. If we want to disable it
-                        # we need to comment the following code and uncomment the next one.
-                        tile = tile.convert_alpha()
-                        screen_x = (x - y) * (tilewidth // 2) + (screen_width // 2)
-                        screen_y = (x + y) * (tileheight // 2)
-                        surface.blit(tile, (screen_x, screen_y + 20))
-                        # surface.blit(tile, (x * self.tmxdata.tilewidth,
-                        #                     y * self.tmxdata.tileheight))
-
-    def make_map(self):
-        # temp_surface = pygame.Surface((self.width, self.height))
-        self.render(screen)
-        # return temp_surface
-    
+pygame.mixer.init()
+clock = pygame.time.Clock()
 
 tmx_map = TiledMap("map/test_map.tmx")
+player = Player(20, 20)
+
+all_sprites = pygame.sprite.Group()
+all_sprites.add(player)
 
 # Game loop
 running = True
@@ -51,14 +19,19 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    # Clear the screen
+    keys = pygame.key.get_pressed()
+    player.update(keys)
+
     screen.fill((0, 0, 0))
 
-    # Draw the map
-    tmx_map.make_map()
+    tmx_map.make_map(screen)
 
+    all_sprites.draw(screen)
+    
     # Update the display
     pygame.display.flip()
+    # pygame.display.update()
+    clock.tick(60)
 
 # Quit Pygame
 pygame.quit()
